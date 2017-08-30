b0d334ad72ca3dbae2bc3c0283e6ad4c384640ba
Brian Fan <kuomu.fan@yahoo.com>
2017-08-29 20:38:34 -0700

    add strong parameters and succesffully added new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-  # def create
-  #   @post = Post.new()
-  # end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      # render the form again 
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
