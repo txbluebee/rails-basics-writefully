bc4ac3d92be2d64af7dac77b258e2dee9e1eb3e7
Brian Fan <kuomu.fan@yahoo.com>
2017-08-30 21:53:52 -0700

    handle error message when entering invalid email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to root_path
     else
-      #
+      render :new
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,}
 end
