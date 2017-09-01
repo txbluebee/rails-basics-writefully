f977693313a14175a0b8d544ae3a69a239e93207
Brian Fan <kuomu.fan@yahoo.com>
2017-08-31 21:23:47 -0700

    add confirmed_at and comfirmation_token column to users. In user Model add callback function to generate confirmation token when user created

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,}
+
+  before_create :generate_confirmaion_token
+
+  def generate_confirmaion_token
+    self.confirmation_token = SecureRandom.hex
+  end
 end
